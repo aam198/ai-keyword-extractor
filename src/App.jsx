9afb53d8028184c0d92a3f82eb69f0c2a544566e
aam198 <aam198@psu.edu>
2023-04-05 08:48:49 -0400

    Inital build

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Container, Box } from '@chakra-ui/react'
+
+const App = () => {
+  return (
+    <Box bg='blue.600' color='white' height='100vh' paddingTop={130}>
+      <h1>My App</h1>
+    </Box>
+  );
+};
+
+export default App;
