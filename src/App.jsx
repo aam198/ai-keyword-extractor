564240bfea5def38cc52990182feff990f3d9de2
aam198 <aam198@psu.edu>
2023-04-12 14:13:14 -0400

    fetch api to request openai based on set paramenters

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,9 @@ const App = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const extractKeywords = (text) => {
+  const extractKeywords = async (text) => {
     console.log(text)
+    // Will open modal and display loading spinner
     setLoading(true)
     setIsOpen(true)
 
@@ -22,9 +23,30 @@ const App = () => {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
       },
-      
+      body: JSON.stringify({
+        model: 'text-davinci-003',
+        prompt: 
+          'Extract keywords from this text. Make the first letter of every word uppercase and separate with commas: \n\n' + 
+          text + '',
+        temperature: 0.5,
+        max_tokens: 60,
+        top_p: 1.0,
+        frequency_penalty: 0.8,
+        presence_penalty: 0.0,
+      }),
     }
-  }
+    
+    try {
+      const response = await fetch(import.meta.env.VITE_OPENAI_API_URL, options);
+      const json = await response.json();
+      console.log(json.choices[0].text.trim());
+      setKeywords(json.choices[0].text.trim());
+      setLoading(false);
+    }
+    catch (error){
+      console.log(error);
+    }
+  };
 
   return (
     <Box bg='blue.600' color='white' height='100vh' paddingTop={130}>
